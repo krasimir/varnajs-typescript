6f4074952dc93f4907d8f91cb133e27a760a3159
Krasimir Tsonev <krasimir@outset.ws>
2020-02-23 08:14:33 +0200

    Examples

--- _examples/01.basicTypes.ts
+++ _examples/01.basicTypes.ts
@@ -2,46 +2,46 @@ const isRegistered = false;
 const answer = 42;
 const place = 'VarnaLab';
 
-const valuesA: number[] = [2, 4, 6];
-const valuesB: Array<string> = ['foo', 'bar'];
+// const valuesA: number[] = [2, 4, 6];
+// const valuesB: Array<string> = ['foo', 'bar'];
 
-// Tuple
-const user: [string, number] = ['Krasimir', 35];
+// // Tuple
+// const user: [string, number] = ['Krasimir', 35];
 
-// Enums
-enum ColorsA {
-  Red,
-  Green,
-  Blue,
-}
-// console.log(ColorsA);
-enum ColorsB {
-  Red = '#F00',
-  Green = '#0F0',
-  Blue = '#00F',
-}
-// console.log(ColorsB);
+// // Enums
+// enum ColorsA {
+//   Red,
+//   Green,
+//   Blue,
+// }
+// // console.log(ColorsA);
+// enum ColorsB {
+//   Red = '#F00',
+//   Green = '#0F0',
+//   Blue = '#00F',
+// }
+// // console.log(ColorsB);
 
-// Any
-let never: any = 'Foo';
-never = 42;
+// // Any
+// let never: any = 'Foo';
+// never = 42;
 
-// functions
-function hello(message: string, name?: string): void {
-  console.log(message, name || 'VarnaJS');
-}
+// // functions
+// function hello(message: string, name?: string): void {
+//   console.log(message, name || 'VarnaJS');
+// }
 
-hello('Hey');
+// hello('Hey');
 
-// overloads
-function eat(param: string): string;
-function eat(param: { who: string; what: string }): string;
-function eat(param: any) {
-  if (typeof param === 'string') {
-    return `I ate ${param}`;
-  }
-  return `${param.who} ate ${param.what}`;
-}
+// // overloads
+// function eat(param: string): string;
+// function eat(param: { who: string; what: string }): string;
+// function eat(param: any) {
+//   if (typeof param === 'string') {
+//     return `I ate ${param}`;
+//   }
+//   return `${param.who} ate ${param.what}`;
+// }
 
-console.log(eat('burger'));
-console.log(eat({ who: 'John', what: 'banana' }));
+// console.log(eat('burger'));
+// console.log(eat({ who: 'John', what: 'banana' }));
